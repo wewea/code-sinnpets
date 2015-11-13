57533189a10698526bd68502c30c1c0e9d076f60
vimquee <vimquee@live.com>
2015-11-13 21:13:05 +0800

    update

--- NodeJS/LearnEventProxy/any.js
+++ NodeJS/LearnEventProxy/any.js
@@ -1,7 +1,7 @@
 var EventProxy = require('eventproxy');
 var ep = new EventProxy();
 
-// trigger if any event which was register
+// trigger if any event which were register, only execute once
 ep.any('ev1', 'ev2', function (data1, data2) {
 	if (data1) {
 		console.log('trigger ev1');
@@ -13,3 +13,5 @@ ep.any('ev1', 'ev2', function (data1, data2) {
 });
 
 ep.emit('ev1');
+ep.emit('ev2');
+
--- NodeJS/LearnEventProxy/tail.js
+++ NodeJS/LearnEventProxy/tail.js
@@ -2,12 +2,12 @@ var Eventproxy = require('eventproxy');
 var ep = new Eventproxy();
 
 ep.tail('event1', 'event2', function (data1, data2) {
-	console.log('trigger tail');
+	console.log('data1: ' + data1 + ' data2: ' + data2);
 });
 
-ep.emit('event1', null);
+ep.emit('event1', 1);
 
 setInterval(function () {
-	ep.emit('event2', null);
+	ep.emit('event2', 2);
 }, 1000);
 
